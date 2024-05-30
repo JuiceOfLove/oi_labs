7af0de36f6754134532aeed6ce8954359bbd2686
Андрей Цыбуленко <andrey_csgo26@mail.ru>
2024-05-30 14:41:09 +0300

    oi

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <Canvas/>
+</template>
+
+<script>
+import Canvas from './components/ui/Canvas/Canvas.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Canvas
+  }
+}
+</script>
+
+<style>
+
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+#app {
+  background-color: #151728;
+  height: 100vh;
+}
+</style>
